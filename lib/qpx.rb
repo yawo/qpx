e614931bb65ed3d13aacc32eecbb1a467b3d335d
Yawo <mcguy2008@gmail.com>
2014-10-22 01:30:19 +0200

    comment configure

--- lib/qpx.rb
+++ lib/qpx.rb
@@ -182,7 +182,7 @@ module Qpx
     end
 
     #Configure defaults
-    self.configure
+    #self.configure
 
 
     ####################################### API Calls ####################################
